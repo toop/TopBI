f91210980ad819e0da5cf4ff821f2a4388e3541a
toop <totopo@163.com>
2013-01-03 16:17:52 +0800

    v 0.1

--- models/factdb.py
+++ models/factdb.py
@@ -7,12 +7,12 @@ class olap_fact_database(Base):
     __tablename__ = 'olap.fact.database'
     id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
     name = Column(String(50),nullable=False,unique=True)
-	db_name = Column(String(50),nullable=False,unique=True)
-	db_login = Column(String(50),nullable=False)
-	db_password Column(String(50),nullable=False) 
-	db_host = Column(String(64),nullable=False)
-	db_port = Column(integer,nullable=False)
-	# db_type = ([('mysql','MySQL' ),('postgres','PostgreSQL' ),('oracle','Oracle' )],'Database type',nullable=False),
+    db_name = Column(String(50),nullable=False,unique=True)
+    db_login = Column(String(50),nullable=False)
+    db_password Column(String(50),nullable=False) 
+    db_host = Column(String(64),nullable=False)
+    db_port = Column(integer,nullable=False)
+    # db_type = ([('mysql','MySQL' ),('postgres','PostgreSQL' ),('oracle','Oracle' )],'Database type',nullable=False),
     #con_url =  Column.function(_connection_get,method = True,type = 'char',string = 'Connection URL',size = 128 )
     #table_ids = one2many('olap.database.tables','fact_database_id','Tables' )
     loaded = Column(boolean,readonly = True )
@@ -31,13 +31,12 @@ class olap_fact_database(Base):
         self.loaded = loaded
 
 	def __repr__(self):
-        return "<olap_fact_database('%s','%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')>" % (self.name,
-																					  self.db_name,
-																					  self.db_login,
-																					  self.db_password,
-																					  self.db_host,
-																					  self.db_port,
-																					  self.db_type,
-																					  self.con_url,
-																					  self.table_ids,
-																					  self.loaded)
+            return "<olap_fact_database('%s','%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')>" % (self.name,
+													self.db_name,
+													self.db_login,
+													self.db_password,
+													self.db_host,
+													self.db_port,
+													self.db_type,										  self.con_url,
+													self.table_ids,
+													self.loaded)
--- models/users.py
+++ models/users.py
@@ -1,22 +1,22 @@
 from sqlalchemy import Column, Integer, String
 from datetime import datetime
 
-class users(Base):
+class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
     name = Column(String,unique=True)
     fullname = Column(String(50),nullable=False)
     email = Column(String(120), unique=True,nullable=False)
-	password = Column(String(50),nullable=False)
-	created_on = Column(String)
+    password = Column(String(50),nullable=False)
+    created_on = Column(String)
 
     def __init__(self, name, fullname, email,password,created_on=None):
         self.name = name
         self.fullname = fullname
         self.password = password
         self.email = email
-		self.created_on = created_on or datetime.now()
+	self.created_on = created_on or datetime.now()
 
     def __repr__(self):
-       return "<users('%s','%s', '%s', '%s')>" % (self.name, self.fullname, self.email, self.password)
+        return "<User('%s','%s', '%s', '%s')>" % (self.name, self.fullname, self.email, self.password)
