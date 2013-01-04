c70451dd5c0f36a2d36bb6eb5a34727251c1ed34
toop <totopo@163.com>
2013-01-04 11:21:13 +0800

    v 0.1

--- database.py
+++ database.py
@@ -12,7 +12,7 @@ Base = declarative_base()
 Base.query = db_session.query_property()
 
 def  init_db():
-    from models import users,factdb
+    from models import users,factdb,olapcube
     Base.metadata.create_all(bind=db_engine)
 
 '''
